19a3319b6451b79744298d63054bb2857a09b52e
lenny <llaska@live.com>
2021-05-31 13:52:35 +1000

    updated some base files

--- server.js
+++ server.js
@@ -1,24 +1,5 @@
 const express = require("express");
 const app = express();
-var session = require('express-session');
-var MemoryStore = require('memorystore')(session)
-var passport = require('passport');
-
-
-
-// Express Session
-app.use(session({
-  cookie: { maxAge: 86400000 },
-  store: new MemoryStore({
-    checkPeriod: 86400000 // prune expired entries every 24h
-  }),
-  resave: false,
-  secret: 'keyboard cat'
-}))
-
-// Passport init
-app.use(passport.initialize());
-app.use(passport.session());
 
 const port = process.env.PORT || 3001;
 const mongoose = require("mongoose");
